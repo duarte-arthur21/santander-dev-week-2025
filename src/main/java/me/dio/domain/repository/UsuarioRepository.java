44b12b50a0838904506783fe233d0a7a0adcc2f4
duarte-arthur21 <ferreiraduarte21@gmail.com>
2025-03-28 04:05:24 -0300

    Add UsuarioRepository interface for user entity persistence

---
+++ src/main/java/me/dio/domain/repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package me.dio.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import me.dio.domain.model.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    
+
+}
